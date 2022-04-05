d7858ae6f038d3fb78f91f0f47e20c4f2e6520d3
Nikita <bigmama93@mail.ru>
2022-04-05 19:20:57 +0300

    SK clear

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-vn#us@kj4h@hfmqnlj4kauex!)su_cn4u-fkvaht=gw8%(-)ym')
+SECRET_KEY SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
