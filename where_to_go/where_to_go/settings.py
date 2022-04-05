4ed0989c95b93bc9cd79d3d3265141b08a05af73
Nikita <bigmama93@mail.ru>
2022-04-05 17:14:14 +0300

    Add model for images end

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vn#us@kj4h@hfmqnlj4kauex!)su_cn4u-fkvaht=gw8%(-)ym'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-vn#us@kj4h@hfmqnlj4kauex!)su_cn4u-fkvaht=gw8%(-)ym')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
