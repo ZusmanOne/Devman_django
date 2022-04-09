4166d44c6e2663f3d8a957f85444d9c062b94646
Nikita <bigmama93@mail.ru>
2022-04-09 17:13:15 +0300

    add preview image in admin-panel

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ['image', 'get_preview', 'number']
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src ='{obj.image.url}' width='100' height='100'>")
+        else:
+            return '-'
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('title',)
     inlines = [ImageInline]
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('number','get_photo')
+    readonly_fields = ('get_photo',)
+
+    def get_photo(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src ='{obj.image.url}' width='70'>")
+        else:
+            return '-'
+
+
 # Register your models here.
